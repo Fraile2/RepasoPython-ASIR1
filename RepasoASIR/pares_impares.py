e788ed1fbb934610ae1d6e90e80475aa175a4a72
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2023-11-23 18:40:29 +0100

    cosas

--- RepasoASIR/pares_impares.py
+++ RepasoASIR/pares_impares.py
@@ -1,5 +1,9 @@
 # num=int(input("Ingrese un numero: "))
 # read -p "Ingrese un numero: " num
+for n in range(1, 11):
+    print(n)
+
+
 for i in range(0, 16, 1):
     if i%2!=1: # numeros impares
         print(i)
