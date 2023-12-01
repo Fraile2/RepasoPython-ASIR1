7db903ee7bbb76bb2b904a1da1e371de6d7042c3
Fraile2 <145047146+Fraile2@users.noreply.github.com>
2023-12-01 16:26:08 +0100

    Update ejemplo.py

--- RepasoASIR/ejemplo.py
+++ RepasoASIR/ejemplo.py
@@ -1,1 +1,14 @@
-restos='TRWAGMYFPDXBNJZSQVHLCKET'
+# restos='TRWAGMYFPDXBNJZSQVHLCKET'
+# dni=input("Introduzca un DNI: ")
+# letra=restos[int(dni)%23]
+# print(letra)
+
+
+# numero=int(input("Introduzca un numero: "))
+# for i in range(1,numero+1):
+#     if numero%i==0:
+#         print(i,"", end="")
+
+msg="HOLA MUNDO"
+msg.replace('A',"4").replace('E',"3").replace('I',"1").replace('O',"0").replace('U',"")
+print(msg)
